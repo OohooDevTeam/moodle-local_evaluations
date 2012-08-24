7d34904a3e80743545a707f81e0e516aaa279634
Andrew McCann <amccann@ualberta.ca>
2012-08-24 10:24:35 -0600

    More refactoring and comments

--- classes/evaluation.php
+++ classes/evaluation.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * ************************************************************************
  * *                              Evaluation                             **
@@ -16,28 +17,31 @@
  * ********************************************************************** */
 
 /**
- * Description of question_set
+ * This class handles all things related to creating and updating evaluations.
  */
 class evaluation {
 
-    private $questionSet = array();
-    private $eval_id = 0;
-    private $deleted = 0;
-    private $course;
-    private $start_time;
-    private $end_time;
-    private $name;
-    private $email_students;
-    private $type = 'online';
-    private $complete = 0;
-    private $dept;
+    private $questionSet = array(); //Evaluation questions.
+    private $eval_id = 0; //The id of the evaluation or 0 if it's new.
+    private $deleted = 0; //Whether or not the evaluation has been deleted.
+    private /* int */ $course; //The course id of the course that the evaluation belongs to. 
+    private $start_time; //The start time of the evaluation.
+    private $end_time; //The end time of the evaluation.
+    private $name; //The name given to the evaluation.
+    private $email_students; //Whether or not to email students to inform them of the evaluation.
+    private $complete = 0; //Whether or not the evaluation is complete.
+    private /* string */$dept; //The department code of the department that the evaluation's course belongs to.
 
-    //If questions included - these will be the ones loaded
-    function __construct($dept, $eval_id = 0, $questions = array(), $course = null, $start_time = null, $end_time = null, $name = null, $email_students = null, $complete = 0, $type = null, $db_load = true) {
+    function __construct($dept, $eval_id = 0, $questions = array(),
+            $course = null, $start_time = null, $end_time = null, $name = null,
+            $email_students = null, $complete = 0, $type = null, $db_load = true) {
         global $DB;
-        $this->dept = $dept;
+
+        $this->dept = $dept; //Set the department.
+
         if ($eval_id != 0) {//Not a new evaluation
-            if ($db_load && $eval_db = $DB->get_record('evaluations', array('id' => $eval_id, 'deleted' => 0))) { //load from database
+            if ($db_load && $eval_db = $DB->get_record('evaluations',
+                    array('id' => $eval_id, 'deleted' => 0))) { //load from database
                 $this->eval_id = $eval_id;
                 $this->course = $eval_db->course;
                 $this->start_time = $eval_db->start_time;
@@ -46,9 +50,8 @@ class evaluation {
                 $this->email_students = $eval_db->email_students;
                 $this->complete = $eval_db->complete;
                 $this->deleted = $eval_db->deleted;
-                //Didn't exist in database not sure what it's supposed to do either. - Someone who inherited uncommented code.
-//                $this->type = $eval_db->type;
-            } elseif (!$db_load && $eval_db = $DB->get_record('evaluations', array('id' => $eval_id, 'deleted' => 0))) { //load from parameters, BUT check if exist
+            } elseif (!$db_load && $eval_db = $DB->get_record('evaluations',
+                    array('id' => $eval_id, 'deleted' => 0))) { //load from parameters, BUT check if exist
                 $this->eval_id = $eval_id;
                 $this->course = $course;
                 $this->start_time = $start_time;
@@ -56,93 +59,149 @@ class evaluation {
                 $this->name = $name;
                 $this->email_students = $email_students;
                 $this->complete = $complete;
-//                $this->type = $type;
             } else {
                 print_error(get_string('eval_id_invalid', 'local_evaluations'));
             }
 
             $this->load_questionSet($questions);
             return;
-        }
-
-        $this->eval_id = $eval_id;
-        $this->course = $course;
-        $this->start_time = $start_time;
-        $this->end_time = $end_time;
-        $this->name = $name;
-        $this->email_students = $email_students;
-        $this->complete = $complete;
-        $this->type = $type;
-
-        if (empty($questions)) {
-            $this->load_standard_questionSet();
         } else {
-            $this->load_questionSet($questions);
+            //If it is a new evaluation.
+            $this->eval_id = $eval_id;
+            $this->course = $course;
+            $this->start_time = $start_time;
+            $this->end_time = $end_time;
+            $this->name = $name;
+            $this->email_students = $email_students;
+            $this->complete = $complete;
+            $this->type = $type;
+
+            if (empty($questions)) {
+                $this->load_standard_questionSet();
+            } else {
+                $this->load_questionSet($questions);
+            }
         }
     }
 
+    /**
+     * Returns the id of this evaluation.
+     * 
+     * @return int  The id of this evaluation.
+     */
     public function get_id() {
         return $this->eval_id;
     }
 
+    /**
+     * Get the course id of the course that this evaluation belongs to.
+     * @return type
+     */
+    function get_course() {
+        return $this->course;
+    }
+
+    /**
+     * Load all standard questions into this evaluations question set.
+     * 
+     * @global moodle_database $DB
+     * @global $CFG
+     */
     function load_standard_questionSet() {
         global $DB, $CFG;
+        //Get all question types.
         $question_types = $DB->get_records('evaluations_question_types');
-        $default_questions = $DB->get_records_select('evaluation_standard_question', 'department = \'' . $this->dept . '\'', null, 'question_order ASC');
 
+        //Find all standard questions for this department.
+        $default_questions = $DB->get_records_select('evaluation_standard_question',
+                'department = \'' . $this->dept . '\'', null,
+                'question_order ASC');
+
+        //Go through each std Question and create a new question object.
         foreach ($default_questions as $order => $default_question) {
 
+            //Get the question type's class.
             $type = $question_types[$default_question->type];
             $question_class = 'question_' . $type->class;
 
+            //Load the question type page if not already loaded.
             require_once("$CFG->dirroot/local/evaluations/classes/question_types/$question_class.php");
 
+            if (!class_exists($question_class)) {
+                print_error(get_string('error_question_type',
+                                'local_evaluations'));
+            }
+
+            //Generate a new question with the question type's class and put it into the question set at the correct place.
             $this->questionSet[$order] = new $question_class(true, 0, $default_question->question, $default_question->type, $default_question->question_order, false);
         }
     }
 
-    function get_course() {
-        return $this->course;
-        
-    }
-    
+    /**
+     * Load the passed in questions into the evaluations question set.
+     * 
+     * @global moodle_database $DB
+     * @global $CFG
+     * @param stdClass[] $questions A set of questions with the following properties:
+     *          'id' -> The id of the question if it already exists otherwise 0.
+     *          'question' -> The question text.
+     *          'type' -> The type of quetsion that it is.
+     *          'question_order' -> The order that the question will be displayed in. (0 - n) 
+     *          'isstd' -> Whether or not the question is a standard question or not.
+     */
     function load_questionSet($questions) {
         global $DB, $CFG;
-        $DB_load = false;
 
+        
         $question_types = $DB->get_records('evaluations_question_types');
 
-        if (empty($questions) && $this->eval_id != 0) {//if no questions included, and eval is not new - load from database
-            if (!$questions = $DB->get_records('evaluation_questions', array('evalid' => $this->eval_id), 'question_order ASC')) {
+
+        if (empty($questions) && $this->eval_id != 0) {
+            //if no questions included, and eval is not new - load from database
+            if (!$questions = $DB->get_records('evaluation_questions',
+                    array('evalid' => $this->eval_id), 'question_order ASC')) {
+                //If there are no questions to be loaded then create an empty question array.
                 $questions = array();
             }
-            $DB_load = true;
         }
 
 
         foreach ($questions as $order => $question) {
 
+            //Make sure it's not a new question that's empty
             if ($question->id == 0 && $question->question == '')
                 continue;
 
+            //Get the question type's class.
             $type = $question_types[$question->type];
             $question_class = 'question_' . $type->class;
 
-            include_once("$CFG->dirroot/local/evaluations/classes/question_types/$question_class.php");
+            //Load the question type page if not already loaded.
+            require_once("$CFG->dirroot/local/evaluations/classes/question_types/$question_class.php");
 
             if (!class_exists($question_class)) {
-                print_error(get_string('error_question_type', 'local_evaluations'));
+                print_error(get_string('error_question_type',
+                                'local_evaluations'));
             }
-
+            
+            //Generate a new question with the question type's class and put it into the question set at the correct place.
             $this->questionSet[$order] = new $question_class($question->isstd, $question->id, $question->question, $question->type, $question->question_order, $DB_load);
         }
     }
 
+    /**
+     * Not sure yet. Will fill in when I come across this method later.
+     * 
+     * @global moodle_database $DB
+     * @param $mform
+     * @param type $form
+     * @param type $data
+     * @param type $include_questions
+     */
     function load_creation_form(&$mform, $form, $data, $include_questions = true) {
         global $DB;
 
         //DATA
-
         $data->eval_complete = $this->complete;
         $data->eval_name = $this->name;
         $data->eval_course_id = $this->course;
@@ -152,7 +211,6 @@ class evaluation {
         $data->eval_time_end = $this->end_time;
         $data->eval_id = $this->eval_id;
         $data->eval_type = trim("$this->type");
-        //print_r($this->questionSet);exit();
 
         if ($include_questions) { // If we are going to include questions - prepare forum and load their data
             $repeatarray = questionCreation_mform($mform);
@@ -161,8 +219,8 @@ class evaluation {
             $repeatno += 1;
 
             $repeateloptions = array();
-
-            $form->repeat_elements($repeatarray, $repeatno, $repeateloptions, 'option_repeats', 'option_add_fields', 1);
+            $form->repeat_elements($repeatarray, $repeatno, $repeateloptions,
+                    'option_repeats', 'option_add_fields', 1);
 
 
 
@@ -173,12 +231,16 @@ class evaluation {
 
 
         $buttonarray = array();
-        $buttonarray[] = &$mform->createElement('submit', 'submitbutton', get_string('complete', 'local_evaluations'));
+        $buttonarray[] = &$mform->createElement('submit', 'submitbutton',
+                        get_string('complete', 'local_evaluations'));
         $buttonarray[] = &$mform->createElement('cancel');
         $mform->addGroup($buttonarray, 'buttonar', '', array(' '), false);
         $mform->closeHeaderBefore('buttonar');
     }
 
+    /**
+     * Saves any changes made to the evaluation object.
+     */
     function save() {
         global $DB;
         $eval = new stdClass();
@@ -209,8 +271,10 @@ class evaluation {
                 $this->start_time = $current;
                 $eval->start_time = $current;
             }
+            //Insert Record and update evalid
             $this->eval_id = $DB->insert_record('evaluations', $eval);
 
+            //Update trigger... I have no idea what triggers acctually do yet.
             if ($this->eval_id > 0) {
 
                 $eventdata = new object();
@@ -229,16 +293,21 @@ class evaluation {
         } else {//update existing eval
             $DB->update_record('evaluations', $eval);
         }
-
+        
+        //Save each question.
         $this->save_questions();
     }
 
+    /**
+     * Save each question in the questionset.
+     */
     function save_questions() {
 
+        //Can't save questions to a new evaluation.
         if ($this->eval_id == 0) {
             print_error(get_string('invalid_evalid_save', 'local_evaluations'));
         }
-        
+
         foreach ($this->questionSet as $question) {
             $question->save($this->eval_id);
         }
@@ -249,7 +318,8 @@ class evaluation {
 
         $context = get_context_instance(CONTEXT_SYSTEM);
         if (is_dept_admin($dept, $USER)) {
-            $mform->addElement('html', '<h3><font color=red>PREVIEW ONLY - WILL NOT BE RECORDED</font></h3>');
+            $mform->addElement('html',
+                    '<h3><font color=red>PREVIEW ONLY - WILL NOT BE RECORDED</font></h3>');
         }
 
         $order = 1;
@@ -262,7 +332,8 @@ class evaluation {
 
         if (is_dept_admin($dept, $USER)) {
             $mform->closeHeaderBefore('end_preview');
-            $mform->addElement('html', '<h3 name="end_preview" id="end_preview"><font color=red>PREVIEW ONLY - WILL NOT BE RECORDED</font></h3>');
+            $mform->addElement('html',
+                    '<h3 name="end_preview" id="end_preview"><font color=red>PREVIEW ONLY - WILL NOT BE RECORDED</font></h3>');
         } else {
             $form->add_action_buttons(false);
         }
